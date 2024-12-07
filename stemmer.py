b5fb4960a9649c86b0bf9f296afdd002c72ee898
Kidanekal <98110702+kidanekalM@users.noreply.github.com>
2024-12-07 14:43:42 +0300

    Create stemmer.py

---
+++ stemmer.py
@@ -0,0 +1,12 @@
+
+
+# stemming function 
+class stemmer :
+    
+    def __init__(self) :
+        pass
+
+    def stem(self, word) :
+        return word
+    def stems(self, words) :
+        return words
