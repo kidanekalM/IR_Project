47d1e769c7358ca0c6378995bdd35448a815e81b
Kidanekal <98110702+kidanekalM@users.noreply.github.com>
2024-12-11 17:38:44 +0300

    Update stemmer.py

--- stemmer.py
+++ stemmer.py
@@ -1,12 +1,29 @@
+from transliterate import transliterate, reverse_transliterate
+from nouns import nouns
+from affixes import prefix, suffix, infix
 
+def stem(word):
+    word = transliterate(word)
 
-# stemming function 
-class stemmer :
+    for pre in prefix:
+        if word.startswith(pre):
+            word = word[len(pre):]
+            break
     
-    def __init__(self) :
-        pass
+    for suf in suffix:
+        if word.endswith(suf):
+            word = word[:-len(suf)]
+            break
+    
+    for inf in infix:
+        if inf in word:
+            word = word.replace(inf, "")
+            break
+    
+    word = reverse_transliterate(word)
+    
+    return word
 
-    def stem(self, word) :
-        return word
-    def stems(self, words) :
-        return words
+stemmed_nouns = [stem(noun) for noun in (nouns[0]).split(" ")]
+
+print(stemmed_nouns)
