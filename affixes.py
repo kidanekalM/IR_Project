7e9eaa83f8a25e6fdf7318caebecde1f1bb962f8
Kidanekal <98110702+kidanekalM@users.noreply.github.com>
2024-12-18 17:31:37 +0300

    Update affixes.py

--- affixes.py
+++ affixes.py
@@ -1,26 +1,49 @@
 
 prefix= [
-        "ze",
-        "wa",
-        "be"
+        "wa", # and 
+        "ze", # Posession of 
+        "be", # place of / while 
+        "y",# ይ Verb - to do
+        "ta",# ታ verb to do for female
+        "i" # negation [ not + verb ]
+
     ]
 suffix= [
-        "ya",
-        "ka",
-        "ki",
-        "kemu",
-        "kene",
+        "ni",
+        "ye", # posession ~ mine
+        "ka",# posession men / you did 
+        "ki", # posession women / you did 
+        "kemu", # posession plural men
+        "kene", 
         "hone",
-        "homu",
-        "hu",
-        "ha",
-        "an",
-        "omu",
-        "e",
-        "he",
-        "u",
-        "en",
-        "I"
+        "he", # posession it's ፍሬሃ 
+        "ku", # I did 
+        "ne", # we did 
+        "homu",# posession plural 
+        "hu", # posession men
+        "ha", # posession men
+        "womu",
+        "omu", # posession plural
+        "e", # posession መንበረ
+        "he", # 
+        "u", # it 2nd person maereru 
+        "en", # they ኃጥኣን
+        "i", # one who performs an action 
+        "yan", # plural one who performs an action 
+        "it", # who performs an action for women 
+        "yat",# plural who performs an action for women
+        "an", # plural men 3rd person
+        "at",# plural who performs an action for women 3rd person
+        
     ]
-infix =[""]
+infix = [
+    "a", # "አ" 
+    "u", # "ኡ" 
+    "i", # "ኢ"
+    "a", # "ኣ"
+    "E", # "ኤ"
+    "I", # "እ"
+    "o", # "ኦ"
+    "e", # "ኧ"
+    ]
 
