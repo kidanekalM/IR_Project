e3ea98c2fa5564190fca92d2a74e5e543c85b7b1
Kidanekal <98110702+kidanekalM@users.noreply.github.com>
2025-01-02 18:29:45 +0300

    Update tokenizer.py

--- tokenizer.py
+++ tokenizer.py
@@ -2,7 +2,7 @@
 import re
 def tokenizer( string_document,sentence_sep="።",word_sep=" "):
 
-    string_document = re.sub(r'[0-9\-]', '', string_document)
+    string_document = re.sub(r'[0-9\-፤፡]', '', string_document)
 
     sentences = string_document.split(sentence_sep)
     words = []
@@ -15,7 +15,7 @@ def tokenizer( string_document,sentence_sep="።",word_sep=" "):
 
 
 
-print(tokenizer("ዘጻድቃን ዘኃጥኣን 1234567765800መዝሙር። ዘዳዊት ብፁዕ ብእሲ4567890 ምክረ ረሲዓን ፍኖተ9876543 ኃጥኣን። መንበረ መስተሳልቃን ሕገ እግዚአብሔር ሥምረቱ መዕልተ ሌሊተ። ዕፅ ሙሓዘ ማይ ፍሬሃ በጊዜሃ መሬት ነፍስ ምድር ምክረ ጻድቃን ፍኖቶሙ ኃጥኣን"))
+# print(tokenizer("ዘጻድቃን ዘኃጥኣን 1234567765800መዝሙር። ዘዳዊት ብፁዕ ብእሲ4567890 ምክረ ረሲዓን ፍኖተ9876543 ኃጥኣን። መንበረ መስተሳልቃን ሕገ እግዚአብሔር ሥምረቱ መዕልተ ሌሊተ። ዕፅ ሙሓዘ ማይ ፍሬሃ በጊዜሃ መሬት ነፍስ ምድር ምክረ ጻድቃን ፍኖቶሙ ኃጥኣን"))
 
 
 
