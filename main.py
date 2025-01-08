28448b2b3c10e0cf2f3a71a0d48e55ae27a57887
Kidanekal <98110702+kidanekalM@users.noreply.github.com>
2025-01-08 12:08:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,54 +4,18 @@ from tkinter.font import Font
 
 from psalms_notes import psalms_notes
 from tokenizer import tokenizer
-from stopword import remove_stopword
-from stemmer import stem
+from stopword import remove_stopwords
+from stemmer import stem_list
 
 #corpus collection > Tokenization > stopowrd removal > stemmer > Weighted Index > Remove terms > Query > UI  
 
-def display_output():
-    input_text = entry.get()
-    stemW,rootW = stem(input_text)
-    output_label.config(text=f"Stem: {stemW} \nRoot: {rootW}")
-
-# Create the main window
-root = tk.Tk()
-root.title("Ge'ez Stemmer")
-root.geometry("400x400")
-root.configure(padx=20, pady=20)
-root.configure(bg="#f0f0f0")  # Light background color
-
-# Style configuration
-style = ttk.Style()
-style.theme_use('clam')  # Use a modern theme
-
-# Button styling
-style.configure("TButton", padding=10, font=("Helvetica", 16), relief="raised", background="#4CAF50", foreground="white")
-style.map("TButton", background=[('active', '#45a049')])
-
-# Label styling
-style.configure("TLabel", font=("Helvetica", 20))
-
-# Create a frame for the input area with rounded edges
-frame = ttk.Frame(root, padding=20)
-frame.pack(pady=10)
-
-# Create an entry widget for text input
-entry = ttk.Entry(frame, width=50)
-entry.pack(pady=10)
-
-# Set a larger font directly on the entry widget
-entry_font = Font(family="Helvetica", size=18)
-entry.config(font=entry_font)
-
-# Create a button to submit the input
-submit_button = ttk.Button(root, text="Submit", command=display_output)
-submit_button.pack(pady=5)
-
-# Create a label to display the output
-output_label = ttk.Label(root, text="Stem: ")
-output_label.pack(pady=10)
-
-
-# Run the application
-root.mainloop()
+stemmed_token=[]
+for psalm in psalms_notes:
+    # print(psalm['text'])
+    tokens = tokenizer(psalm['text'])
+    stopword_removed_token = remove_stopwords(tokens)
+    stemmed_token = stem_list(stopword_removed_token)
+    break
+    # print(tokens)
+    # print(stopword_removed_token)
+print("stemmed", stemmed_token)
