47415565445329fc8536048339395e744cf84c9e
Kidanekal <98110702+kidanekalM@users.noreply.github.com>
2025-01-08 11:41:47 +0300

    Update stopword.py
    FIxed it by changing a single word from if to while

--- stopword.py
+++ stopword.py
@@ -1,4 +1,6 @@
 from psalms_notes import psalms_notes
+from psalms_notes import psalms_notes
+from tokenizer import tokenizer
 
 stopwords=[
     "ውስተ",
@@ -9,8 +11,15 @@ stopwords=[
     'Psalms'
 ]
 
-def remove_stopwords(text):
-    return ' '.join([word for word in text.split() if word not in stopwords])
+def remove_stopwords(tokens):
+    # return ' '.join([word for word in text.split() if word not in stopwords])
+    for stopword in stopwords:
+        while stopword in tokens:
+            tokens.remove(stopword) 
+    return tokens
 
-print (remove_stopwords(psalms_notes[0]['text']))
-print(psalms_notes[0]['text'])
+# print (remove_stopwords(psalms_notes[0]['text']))
+# print(psalms_notes[0]['text'])
+tokens  = tokenizer(psalms_notes[0]['text'])
+print(tokens)
+print(remove_stopwords(tokens))
