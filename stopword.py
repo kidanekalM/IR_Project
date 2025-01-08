8eca28a8f9d42c5632ea210ebb3c2366a6892cbb
Kidanekal <98110702+kidanekalM@users.noreply.github.com>
2025-01-08 12:08:41 +0300

    Update stopword.py

--- stopword.py
+++ stopword.py
@@ -12,14 +12,11 @@ stopwords=[
 ]
 
 def remove_stopwords(tokens):
-    # return ' '.join([word for word in text.split() if word not in stopwords])
     for stopword in stopwords:
         while stopword in tokens:
             tokens.remove(stopword) 
     return tokens
 
-# print (remove_stopwords(psalms_notes[0]['text']))
-# print(psalms_notes[0]['text'])
-tokens  = tokenizer(psalms_notes[0]['text'])
-print(tokens)
-print(remove_stopwords(tokens))
+# tokens  = tokenizer(psalms_notes[0]['text'])
+# print(tokens)
+# print(remove_stopwords(tokens))
