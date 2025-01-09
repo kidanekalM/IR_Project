fc688878a946e7d9bb20c35821979bc6ecd7cae1
Kidanekal <98110702+kidanekalM@users.noreply.github.com>
2025-01-09 17:58:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -11,21 +11,20 @@ from indexer import create_index_with_weights
 #corpus collection > Tokenization > stopowrd removal > stemmer > Weighted Index > Remove terms > Query > UI  
 
 stemmed_tokens =[]
-count = 0
+# count = 0
 for psalm in psalms_notes:
-    # print(psalm['text'])
     tokens = tokenizer(psalm['text'])
     stopword_removed_token = remove_stopwords(tokens)
     stemmed_token = stem_list(stopword_removed_token)
-    stemmed_tokens.append({'doc_id':psalm['doc_id'],'tokens':stemmed_token})
-    # stemmed_tokens['doc_id'] = psalm['doc_id']
 
-    # stemmed_tokens[stemmed_tokens['doc_id']['tokens']] = stemmed_token
-    if count > 5:
-        break
-    count += 1 
-print(stemmed_tokens)
-print("Weighted ",create_index_with_weights(stemmed_tokens))
-    # print(tokens)
-    # print(stopword_removed_token)
-# print("prodess " , stemmed_tokens)
+    stemmed_tokens.append({'doc_id':psalm['doc_id'],'tokens':stemmed_token})
+
+inverted_index = create_index_with_weights(stemmed_tokens)
+
+with open('inverted_index.py','w', encoding='utf-8') as file:
+    file.append(f"inverted_index = {inverted_index}")
+    
+    # if count > 1:  break; count += 1 
+
+
+# [print(element) for element in create_index_with_weights(stemmed_tokens)]
