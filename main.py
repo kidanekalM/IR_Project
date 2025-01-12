9b72ff270708cdbcd1b71e664297716c0d9da7e2
Kidanekal <98110702+kidanekalM@users.noreply.github.com>
2025-01-12 18:50:36 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import subprocess
 import tkinter as tk
 from tkinter import ttk
 from tkinter.font import Font
+
+from inverted_index import inverted_index
 
 from psalms_notes import psalms_notes
 from tokenizer import tokenizer
@@ -8,24 +11,29 @@ from stopword import remove_stopwords
 from stemmer import stem_list
 from collections import defaultdict
 from indexer import create_index_with_weights
+from matching import matching
 #corpus collection > Tokenization > stopowrd removal > stemmer > Weighted Index > Remove terms > Query > UI  
+def preprocess():
+    stemmed_tokens =[]
+    # count = 0
+    for psalm in psalms_notes:
+        tokens = tokenizer(psalm['text'])
+        stopword_removed_token = remove_stopwords(tokens)
+        stemmed_token = stem_list(stopword_removed_token)
 
-stemmed_tokens =[]
-# count = 0
-for psalm in psalms_notes:
-    tokens = tokenizer(psalm['text'])
-    stopword_removed_token = remove_stopwords(tokens)
-    stemmed_token = stem_list(stopword_removed_token)
+        stemmed_tokens.append({'doc_id':psalm['doc_id'],'tokens':stemmed_token})
 
-    stemmed_tokens.append({'doc_id':psalm['doc_id'],'tokens':stemmed_token})
+    inverted_index = create_index_with_weights(stemmed_tokens)
 
-inverted_index = create_index_with_weights(stemmed_tokens)
-
-# with open('inverted_index.py','w', encoding='utf-8') as file:
-    # file.write(f"inverted_index = {inverted_index}")
+    with open('inverted_index.py','w', encoding='utf-8') as file:
+        file.write(f"inverted_index = {inverted_index}")
      
-    # if count > 1:  break; count += 1 
+    return inverted_index
 
-from inverted_index import inverted_index
-print(inverted_index['ወሬዛ'])
+# print(matching('ወእመአኮ ከመ ወሬዛ ሃየል',inverted_index))
+
+
+result = subprocess.run(['streamlit', 'run', 'ui.py'], capture_output=True, text=True)
+
+print(result.stdout)
 
