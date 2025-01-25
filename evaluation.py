a318005d54aaa19732c60d25a5dfe5daa0b9ddcb
Kidanekal <98110702+kidanekalM@users.noreply.github.com>
2025-01-25 13:36:23 +0300

    added f1

--- evaluation.py
+++ evaluation.py
@@ -3,5 +3,13 @@ def precision(relevant_docs, retrieved_docs):
     if retrieved_docs == 0:
         return 0
     return relevant_docs / retrieved_docs
-def recall(relevant_docs, retrieved_docs):
-    return  0
+def recall(relevant_docs, total_rel):
+    if total_rel == 0:
+        return 0
+    return relevant_docs/total_rel
+def f1(relevant_docs,retrieved_docs,total_rel):
+    p = precision(relevant_docs,retrieved_docs)
+    r =  recall(relevant_docs,total_rel)
+    if (p+r) == 0:
+        return 0
+    return (2*p*r)/(p+r)
